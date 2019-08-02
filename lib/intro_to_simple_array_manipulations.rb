bb30400f39b20a3d40ffacb5aabf42609b324281
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-08-02 21:43:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,7 @@ artist=["Hozier", "Ariana Grande", "Usher",]
 next_artist = "Beyonce"
 artists.push(next_artist)
 # => {"Hozier", "Ariana Grande", "Usher",
+artists = ["Frida Kahlo", "Pablo Piccasso", "Cai Guo-Qiang"]
+artists.pop
+# => "Cai Guo-Qiang"
 
